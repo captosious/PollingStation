2e2bcdeb748fce87d996597273e738db07b4bc80
codeforgery <pedro.rio@me.com>
2019-10-19 21:56:36 +0200

    Inserción inicial en el repositorio del módulo PollingStationSolution

---
+++ PollingStationWebAPI/Organization.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PollingStationWebAPI
+{
+    public class Organization
+    {
+        public int organization_id { get; set; }
+        public string  name { get; set; }
+        public string info_email { get; set; }
+        public string robot_email { get; set; }
+
+    }
+}
